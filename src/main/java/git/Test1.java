06f958783b38890964166a51429a2e0a1c5f1dd8
HADcoder <yangxiaokai1314@163.com>
2018-10-27 19:35:09 +0800

    2 程序员王楠对项目源码作出了修改,提交版本2

--- src/main/java/git/Test1.java
+++ src/main/java/git/Test1.java
@@ -3,5 +3,6 @@ package git;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("1 程序员杨凯强提交项目源码");
+        System.out.println("2 程序员王楠对项目源码作出了修改,提交版本2");
     }
 }
