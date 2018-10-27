b9122b073cf6c6787c9c9e3f3da94252eb325f0c
HADcoder <yangxiaokai1314@163.com>
2018-10-27 19:42:17 +0800

    这是一个测试分支

--- src/main/java/git/Test1.java
+++ src/main/java/git/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("1 程序员杨凯强提交项目源码");
         System.out.println("2 程序员王楠对项目源码作出了修改,提交版本2");
+        System.out.println("3 这是一个测试分支");
+
     }
 }
