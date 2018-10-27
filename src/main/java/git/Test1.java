01bad66e4329b61fcce6f181475c53d8875692d7
HADcoder <yangxiaokai1314@163.com>
2018-10-27 19:50:58 +0800

    测试分支第二次更新代码

--- src/main/java/git/Test1.java
+++ src/main/java/git/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
         System.out.println("1 程序员杨凯强提交项目源码");
         System.out.println("2 程序员王楠对项目源码作出了修改,提交版本2");
         System.out.println("3 这是一个测试分支");
+        System.out.println("4 测试分支第二次更新代码");
 
     }
 }
